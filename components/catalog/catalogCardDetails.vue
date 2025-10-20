0decdc609e9fa72feb0e58553a3a82ceff6a982b
vlad <vlad3eko@gmail.com>
2025-10-20 15:23:11 +0300

    Edit Json Catalog.js

--- components/catalog/catalogCardDetails.vue
+++ components/catalog/catalogCardDetails.vue
@@ -5,7 +5,8 @@
     <NuxtImg :src="card.img" :alt="card.name" class="w-full rounded-2xl overflow-hidden mix-blend-multiply"/>
     </div>
     <div class="text-left">
-      <p class="text-2xl font-bold uppercase"><span class="text-[#c4c4c4]">Стоимость: </span>{{ card.price }} ₽</p>
+        <span class="text-[#c4c4c4] block text-3xl text-center uppercase border-b pb-5 mt-5">Стоимость: </span>
+      <p class="font-bold text-center mb-5 mt-5 tracking-widest text-4xl">{{ card.price }} ₽</p>
       <span class="text-[#c4c4c4] block text-3xl text-center uppercase border-b pb-5">Описание</span>
       <div v-for="descr in card.description">
         <p class="text-3xl font-bold text-center mb-5 mt-5">{{ descr.title }}</p>
@@ -24,6 +25,7 @@
 <script setup>
 
 defineProps(['card'])
+
 
 </script>
 
