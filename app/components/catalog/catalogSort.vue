e72b3cef7d2c941778ddff812e59884adbdf2021
vlad <vlad3eko@gmail.com>
2025-12-19 23:24:08 +0300

    fix bugs

--- app/components/catalog/catalogSort.vue
+++ app/components/catalog/catalogSort.vue
@@ -2,13 +2,13 @@
   <div class="p-2 flex items-center ">
     <div v-for="card in cards" :key="card.id">
     </div>
-    <form class="theme-panels relative">
-      <select form="selectCategory" class="text-center pl-2 pr-2 theme-text theme-panels absolute top-2.5 right-2">
+    <form class="border border-black rounded-2xl relative">
+      <select form="selectCategory" class="text-center theme-text theme-panels absolute top-0 right-0 pl-5 h-full rounded-2xl">
         <option @click="onSelectCategory(category)" :value="category" v-for="(category, index) in categories"
                 :key="index">{{ category }}
         </option>
       </select>
-      <input type="text" v-model="search" placeholder="название товара.." class="border pl-5 pr-40 pt-2 pb-2 rounded-2xl">
+      <input type="text" v-model="search" placeholder="название товара.." class="border pl-5 pr-40 pt-2 pb-2 rounded-2xl theme-panels">
     </form>
   </div>
 </template>
