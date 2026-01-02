39e6741cbe9803e6e07fb570176560c13ed9f305
vlad <vlad3eko@gmail.com>
2026-01-03 02:14:26 +0300

    refactor scss styles code add theme-change hook, delete theme-selectors. change themesMode.scss. Edit style of web design

--- app/components/catalog/catalogSort.vue
+++ app/components/catalog/catalogSort.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="pt-2 pb-2 flex items-center">
+  <div class="pt-2 pb-2 flex items-center ">
     <div v-for="card in cards" :key="card.id">
     </div>
-    <form class="border border-black rounded-2xl relative">
-      <select form="selectCategory" class="text-center theme-text theme-panels absolute top-0 right-0 pl-5 scale-x-95 scale-y-90 h-full rounded-2xl">
+    <form class=" rounded-2xl relative ">
+      <select form="selectCategory" class="text-center bg-main accent-active absolute top-0 right-0 pl-5 scale-x-95 scale-y-90 h-full rounded-2xl">
         <option @click="onSelectCategory(category)" :value="category" v-for="(category, index) in categories"
                 :key="index">{{ category }}
         </option>
       </select>
-      <input type="text" v-model="search" placeholder="название товара.." class="border pl-5 pr-40 pt-2 pb-2 rounded-2xl theme-panels">
+      <input type="text" v-model="search" placeholder="название товара.." class="border-top border-left border-right border-bottom pl-5 pr-40 pt-2 pb-2 rounded-2xl bg-main">
     </form>
   </div>
 </template>
