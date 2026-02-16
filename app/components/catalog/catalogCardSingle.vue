b258896e9ea014869d9b08a976b20963a8244347
vlad3eko <vlad3eko@gmail.com>
2026-02-16 18:39:41 +0300

    adaptive design Menu

--- app/components/catalog/catalogCardSingle.vue
+++ app/components/catalog/catalogCardSingle.vue
@@ -6,15 +6,15 @@
         <NuxtImg :src="props.card.img" class="h-full object-cover mx-auto"/>
       </div>
       <div class="m-5">
-        <div class="grid justify-items-stretch grid-cols-3 font-bold border-muted-bottom pb-2 mb-2 rounded">
+        <div class="font-bold border-muted-bottom pb-2 mb-2 rounded">
           <div class="col-span-2 text-left text-2xl">
             {{ props.card.product }}
           </div>
-          <div class="text-right col-start-3 self-center">
+          <div class="text-left ">
           <span v-if="!isRange(props.card.price)">
                   <p class="text-2xl text-price tracking-[.25em]">{{ props.card.price }}</p>
                 </span>
-            <span v-else class="flex justify-end gap-2 items-center">
+            <span v-else class="flex justify-start gap-2 items-center">
                   <p class="text-price-sale tracking-[.15em]">{{ props.card.price[0] }}</p> / <p
                 class="text-2xl text-price tracking-[.15em]">{{ props.card.price[1] }}</p>
                 </span>
