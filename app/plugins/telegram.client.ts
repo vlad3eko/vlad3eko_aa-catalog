eca9248034248cf3b3d386b04fe6f09b56b30221
vlad3eko <vlad3eko@gmail.com>
2026-02-18 23:16:04 +0300

    telegram try

--- app/plugins/telegram.client.ts
+++ app/plugins/telegram.client.ts
@@ -2,12 +2,15 @@ import {useTelegramStore} from "~/store/telegram.store";
 
 export default defineNuxtPlugin(() => {
     const telegramStore = useTelegramStore()
+    console.log('telegramStore', telegramStore)
 
-    const tg = window?.Telegram.Webapp
+    const tg = window?.Telegram.WebApp
+    console.log('tg', tg)
 
     if (tg) {
         tg.ready()
         const user = tg.initDataUnsafe?.user
+        console.log('user', user)
 
         if (user) telegramStore.setUser(user)
     }
