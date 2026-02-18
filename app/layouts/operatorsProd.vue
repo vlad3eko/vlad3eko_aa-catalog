8bf30600234a2a27398ba0522e8dd0adc713d235
vlad3eko <vlad3eko@gmail.com>
2026-02-19 00:01:12 +0300

    telegram try

--- app/layouts/operatorsProd.vue
+++ app/layouts/operatorsProd.vue
@@ -9,12 +9,19 @@
               <basket-sidebar/>
             </div>
             <Menu/>
+            <div
+                v-if="telegramStore.user"
+                class="max-w-10 rounded-4xl overflow-hidden self-center">
+              <img src="https://t.me/i/userpic/320/QxFjFXL2ezATNOY7K4LhJlCl7NBsSiCJjuU5BXZIjXM.svg" alt="Аватар"/>
+            </div>
           </div>
         </div>
-        <p>Имя: {{telegramStore.user.first_name}}</p>
-        <p>Логин: {{telegramStore.user.username}}</p>
-        <img :src="telegramStore.user.photo_url" alt="аватар">
-        {{telegramStore.user}}
+        <div v-if="telegramStore.user" class="flex">
+          <p>Имя: {{ telegramStore.user.first_name }}</p>
+          <p>Логин: {{ telegramStore.user.username }}</p>
+          <img :src="telegramStore.user.photo_url" alt="аватар">
+        </div>
+        {{ telegramStore.user }}
         <slot/>
       </div>
     </div>
--- app/plugins/telegram.client.ts
+++ app/plugins/telegram.client.ts
@@ -2,15 +2,12 @@ import {useTelegramStore} from "~/store/telegram.store";
 
 export default defineNuxtPlugin(() => {
     const telegramStore = useTelegramStore()
-    console.log('telegramStore', telegramStore)
 
-    const tg = window?.Telegram.WebApp
-    console.log('tg', tg)
+    const tg = (window as any).Telegram.WebApp
 
     if (tg) {
         tg.ready()
         const user = tg.initDataUnsafe?.user
-        console.log('user', user)
 
         if (user) telegramStore.setUser(user)
     }
