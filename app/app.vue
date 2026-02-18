fb5ceb71e69ed2c300ff8c68dfd72cd7d936b182
vlad3eko <vlad3eko@gmail.com>
2026-02-18 19:22:05 +0300

    telegram try

--- app/app.vue
+++ app/app.vue
@@ -1,13 +1,14 @@
 <template>
     <NuxtLayout>
-       Добро пожаловать {{tg.first_name}}
-      ваш язык {{tg.language_code}}
+       <p>Добро пожаловать {{tg.first_name}}</p>
+      <p>ваш язык {{tg.language_code}}</p>
       <NuxtImg :src="tg.photo_url"/>
+      <p>{{tg}}</p>
       <NuxtPage/>
     </NuxtLayout>
 </template>
 <script setup lang="ts">
 
 
-const tg = window?.Telegram.WebApp.initDataUnsafe
+const tg = window.Telegram.WebApp.initDataUnsafe
 </script>
