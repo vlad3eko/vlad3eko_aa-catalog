eb831502409d3ebd6752ddae7453bd5bcdcb68b8
vlad3eko <vlad3eko@gmail.com>
2026-02-19 00:12:21 +0300

    telegram try

--- app/layouts/operatorsProd.vue
+++ app/layouts/operatorsProd.vue
@@ -3,25 +3,25 @@
     <div class="bg-background text-foreground">
       <div class="container mx-auto">
         <div class="sticky top-0 z-3">
-          <div class="flex justify-between bg-background animate-from-top">
+          <div class="flex justify-between px-5 bg-background animate-from-top">
             <div class="flex items-center">
               <change-theme/>
               <basket-sidebar/>
             </div>
-            <Menu/>
-            <div
-                v-if="telegramStore.user"
-                class="max-w-10 rounded-4xl overflow-hidden self-center">
-              <img src="https://t.me/i/userpic/320/QxFjFXL2ezATNOY7K4LhJlCl7NBsSiCJjuU5BXZIjXM.svg" alt="Аватар"/>
+            <div class="flex">
+              <Menu/>
+              <div
+                  class="max-w-10 rounded-4xl overflow-hidden self-center">
+                <img
+                    v-if="telegramStore.user"
+                    src="https://t.me/i/userpic/320/QxFjFXL2ezATNOY7K4LhJlCl7NBsSiCJjuU5BXZIjXM.svg" alt="Аватар"/>
+                <img
+                    v-else
+                    src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="Аватар"/>
+              </div>
             </div>
           </div>
         </div>
-        <div v-if="telegramStore.user" class="flex">
-          <p>Имя: {{ telegramStore.user.first_name }}</p>
-          <p>Логин: {{ telegramStore.user.username }}</p>
-          <img :src="telegramStore.user.photo_url" alt="аватар">
-        </div>
-        {{ telegramStore.user }}
         <slot/>
       </div>
     </div>
