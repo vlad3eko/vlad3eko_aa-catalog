e98cb4ad2437d5906542c64c65bdac74a099de03
vlad3eko <vlad3eko@gmail.com>
2026-02-19 14:35:26 +0300

    telegram try

--- app/layouts/operatorsProd.vue
+++ app/layouts/operatorsProd.vue
@@ -10,14 +10,15 @@
             </div>
             <div class="flex">
               <Menu/>
-              <div
-                  class="max-w-10 rounded-4xl overflow-hidden self-center">
-                <img
-                    v-if="telegramStore?.user?.id"
-                    src="https://t.me/i/userpic/320/QxFjFXL2ezATNOY7K4LhJlCl7NBsSiCJjuU5BXZIjXM.svg" alt="Аватар"/>
-                <img
-                    v-else
-                    src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="Аватар"/>
+              <div class="max-w-10 rounded-4xl overflow-hidden self-center">
+                <div v-if="telegramStore?.user?.id">
+                  <img :src="telegramStore.user.photo_url"
+                       alt="Аватар"/>
+                </div>
+                <div v-else>
+                  <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+                       alt="Аватар"/>
+                </div>
               </div>
             </div>
           </div>
--- app/store/telegram.store.ts
+++ app/store/telegram.store.ts
@@ -2,7 +2,14 @@ import {defineStore} from "pinia";
 import type {ITelegramTypes} from "~~/server/utils/telegram/telegram.types";
 
 export const useTelegramStore = defineStore('telegram', () => {
-    const user = ref<any>(null)
+    const user = ref<any>({
+            id: 0,
+            first_name: "",
+            last_name: "",
+            username: "",
+            photo_url: ""
+        })
+
 
     const setUser = (telegramUser: any) => {
         user.value = telegramUser
